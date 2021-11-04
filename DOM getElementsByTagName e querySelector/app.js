40e2b72b0c232cc73ec4d5088faff38f1601ab26
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-04 19:15:35 -0300

    update getElementsByTagName

---
+++ DOM getElementsByTagName e querySelector/app.js
@@ -0,0 +1,11 @@
+console.log(
+    document.getElementsByTagName("div")    
+)
+
+console.log(
+    document.getElementsByTagName("div")[0].innerHTML    
+)
+
+console.log(
+    document.getElementsByTagName("br")   
+)
