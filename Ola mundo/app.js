80a8c2082bc77fadf9990dd49c2ecae080506fd5
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-04 18:33:06 -0300

    using BOM commands

--- Ola mundo/app.js
+++ Ola mundo/app.js
@@ -1,1 +1,31 @@
+/* Comandos BOM (Browser Object Model) */
+
 window.alert("Olá mundo")
+window.console.log(
+    window.document.getElementById("titulo").innerHTML    
+)
+
+console.log(window)
+
+console.log(screen)
+console.log(screen.width)
+console.log(screen.height)
+console.log(screen.orientation)
+console.log(screen.orientation.type)
+
+console.log(navigator)
+console.log(navigator.language)
+
+console.log(location)
+console.log(location.href)
+/*location.href = "https://www.google.com" redireciona para outra pagina*/
+
+console.log(history)
+function voltar(){
+    history.back()
+}
+function adiantar(){
+    history.forward()
+}
+
+
