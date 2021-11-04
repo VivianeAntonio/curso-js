61ddf315ac31735536d8aae99996f61841b3381a
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-04 18:55:02 -0300

    upload exercise

---
+++ DOM getElementById & getElementsByClassName/app.js
@@ -0,0 +1,11 @@
+console.log(
+    document.getElementById("fruta").innerHTML
+)
+
+console.log(
+    document.getElementsByClassName("carro")
+)
+
+console.log(
+    document.getElementsByClassName("carro")[2].innerHTML
+)
