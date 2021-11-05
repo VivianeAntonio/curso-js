48fa9ac131be0fdca659444468ce1f4689345fb5
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-05 15:50:05 -0300

    update innerHTML, Style e Atributos

---
+++ DOM innerHTML, Style e Atributos/app.js
@@ -0,0 +1,19 @@
+console.log(
+    document.getElementById("p1").innerHTML
+)
+
+var paragrafo = document.getElementById("p1")
+paragrafo.innerHTML = "Novo conteúdo"
+
+paragrafo.style.color = "red"
+paragrafo.style.backgroundColor = "#333"
+paragrafo.style.height = "100px"
+
+
+var imagem = document.getElementById("imagem")
+imagem.src = "html5.jpg"
+imagem.alt = "Teste de imagem"
+imagem.width = "400"
+imagem2.src = "javascript.png"
+imagem2.width = "400"
+
