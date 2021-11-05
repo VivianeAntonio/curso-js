6bf544d3f2ba8fd24d75b0b7430c386134ee6dfe
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-05 19:59:01 -0300

    Tipos de dados

---
+++ Tipos de dados/app.js
@@ -0,0 +1,20 @@
+// Tipos de dados primitivos (tipagem dinâmica)
+
+// string
+var nome = "Viviane Antonio"
+// number
+var idade = 20
+// boolean
+var solteiro = false
+
+// array
+var frutas = ["banana", "maça", "goiaba"]
+
+// object
+var carro = new Object()
+carro.fabricacao = "2025"
+carro.cor = "azul"
+
+// function
+var soma = function(a,b) {return a+b}
+alert(soma(9,1))
