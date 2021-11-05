be2784a6d3dd9004bd332fcb3b08442ecfaa4160
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-04 22:31:58 -0300

    update querySelectorAll

---
+++ DOM querySelectorAll/app.js
@@ -0,0 +1,13 @@
+// querySelectorAll retorna todos os objetos NodeList //
+
+console.log(
+    document.querySelectorAll(".times")
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")[0].innerHTML
+)
