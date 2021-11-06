234b10a61adb45f0f35c36b90d0d348af3b8fc43
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-05 21:44:29 -0300

    array

---
+++ Arrays/app.js
@@ -0,0 +1,11 @@
+// Array 
+// É uma estrutura de dados que armazena uma coleção de elementos
+
+let meuarray = [
+    'notebook', 
+    1500, 
+    {memoria: '16gb', hd: '100gb'},
+    ['notebook dell', 'modelo 1000']
+]
+console.log(meuarray[2].memoria)
+console.log(meuarray[3][1])
